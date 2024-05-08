f9c3c2d92514f550978a64f3beceba84a2b27126
Ariling <97501226+Ariling@users.noreply.github.com>
2024-05-08 14:35:32 +0900

    setting : 라이브러리 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        //10분동안은 캐시된 결과를 사용
+        staleTime: 1000 * 60 * 3,
+        //실패 요청해도 비활성화
+        retry: 0,
+      },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
 }
