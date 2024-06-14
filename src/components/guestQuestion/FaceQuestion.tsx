b6e1dc2cf8571e4cc8f06361b74a98e50e64b608
Ariling <97501226+Ariling@users.noreply.github.com>
2024-06-14 14:52:08 +0900

    refactor : structure 비슷한 component 재사용 구조로 바꾸기

--- src/components/guestQuestion/FaceQuestion.tsx
+++ src/components/guestQuestion/FaceQuestion.tsx
@@ -3,6 +3,7 @@ import GuestImage from '../utils/GuestImage'
 import { ContentBox, QuestionBtn } from '@/styles/questionStyle';
 import { useGetSuffix } from '@/hooks/useGetSuffix';
 import { FaceArray } from '../utils/questionArray';
+import QuestionContent from '../utils/QuestionContent';
 
 const FaceQuestion = () => {
     const testSrc = 'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_1280.jpg';
@@ -10,21 +11,20 @@ const FaceQuestion = () => {
   return (
     <>
         <GuestImage src={testSrc}/>
-        <ContentBox>
-            <div className='mt-2 font-Neo font-bold text-[20px] text-center'>
-            {testName}
-          {useGetSuffix(testName)}
+        <QuestionContent 
+        children2={FaceArray.map((e, idx)=>{
+            return(
+                <QuestionBtn key={e}>{e}</QuestionBtn>
+            )
+        })}
+        >
+        <>
+                    {testName}
+        {useGetSuffix(testName)}
           <br />
           00상이야!{" "}
-            </div>
-            <div className='grid grid-cols-2 gap-4'>
-                {FaceArray.map((e, idx)=>{
-                    return(
-                        <QuestionBtn key={e}>{e}</QuestionBtn>
-                    )
-            })}
-            </div>
-        </ContentBox>
+            </>
+        </QuestionContent>
     </>
   )
 }
