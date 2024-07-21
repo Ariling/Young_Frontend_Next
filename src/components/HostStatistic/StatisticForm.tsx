131bdadf9cdd687a2b7de70499bcdf939bf20afe
조민규 <ariettys2@naver.com>
2024-07-21 21:34:09 +0900

    refactor : 중복 컴포 map함수로 리팩토링 및 컴포넌트 분리

---
+++ src/components/HostStatistic/StatisticForm.tsx
@@ -0,0 +1,47 @@
+import { WhiteBox } from "@/styles/hostStyle";
+import { Istatistics } from "@/types/Tstatistic";
+import React from "react";
+import StatisticResult from "./StatisticResult";
+import {
+  ColorArray,
+  EmojiArray,
+  FaceArray,
+  firstImpressionArray,
+  presentImpressionArray,
+} from "@/constants/questionArray";
+import useGetResultTitleArray from "@/hooks/useGetResultTitleArray";
+
+const StatisticForm = ({
+  data,
+  hostNickname,
+  hostSuffixArray,
+}: {
+  data: Istatistics;
+  hostNickname: string;
+  hostSuffixArray: string[];
+}) => {
+  const array = [data.animal, data.emoji, data.color, data.first, data.now];
+  const titleArray = [
+    FaceArray,
+    EmojiArray,
+    ColorArray,
+    firstImpressionArray,
+    presentImpressionArray,
+  ];
+  const questionArray = useGetResultTitleArray(hostNickname, hostSuffixArray);
+  return (
+    <WhiteBox className=" font-Neo" isStatistic>
+      {array.map((e, i) => {
+        return (
+          <>
+            <StatisticResult key={i} result={e} titleArray={titleArray[i]}>
+              <>{questionArray[i]}</>
+            </StatisticResult>
+          </>
+        );
+      })}
+    </WhiteBox>
+  );
+};
+
+export default StatisticForm;
