f2ef7edd51313aaf15fdb897b63dec6145d3719d
조민규 <ariettys2@naver.com>
2024-07-26 22:36:45 +0900

    fix : 컬러 btn에서 동적 스타일링 되지 않는 부분 수정

--- src/components/guestQuestion/ColorQuestion.tsx
+++ src/components/guestQuestion/ColorQuestion.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import QuestionContent from "../common/QuestionContent";
 import { QuestionBtn } from "@/styles/questionStyle";
-import { ColorArray, ColorObject } from "../../constants/questionArray";
+import { ColorArray } from "../../constants/questionArray";
 import { useGetSuffix } from "@/hooks/useGetSuffix";
 import { ReducerProps } from "@/types/Treducer";
 import { useQuestionStore } from "@/store/question";
 import SurveyImageCompo from "../utils/SurveyImageCompo";
 import useGetGuestRoute from "@/hooks/useGetGuestRoute";
+
+// 동적 스타일링을 하려면 이렇게 미리 가져와야 한다. 빌드 시점에 미리 유틸리티 클래스를 감지해 빌딩하기 때문
+const ColorObject = {
+  빨간색: "text-red-600",
+  노란색: "text-yellow-300",
+  초록색: "text-green-600",
+  파란색: "text-blue-700",
+  보라색: "text-purple-700",
+  분홍색: "text-pink-400",
+  흰색: "text-white text-stroke-white",
+  검은색: "text-black",
+};
 
 const ColorQuestion = (props: ReducerProps) => {
   const info = useGetGuestRoute();
@@ -20,7 +32,7 @@ const ColorQuestion = (props: ReducerProps) => {
           return (
             <QuestionBtn
               key={e}
-              className={`${ColorObject[idx]}`}
+              className={`${ColorObject[e as keyof typeof ColorObject]}`}
               onClick={() => {
                 changeQuestion(2, idx);
                 changeImgCode(String(idx + 1), 0);
--- src/pages/hostdeploy/hostStatistic/index.tsx
+++ src/pages/hostdeploy/hostStatistic/index.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Footer from "@/components/common/Footer";
 import NicknameTitle from "@/components/utils/NicknameTitle";
