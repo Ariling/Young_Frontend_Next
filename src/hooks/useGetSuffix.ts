4e771d6733f64a4385cec902ce0fd91b9e2dbcbd
조민규 <ariettys2@naver.com>
2024-07-30 00:32:20 +0900

    !Hotfix : undefined 반환 에러 해결

--- src/hooks/useGetSuffix.ts
+++ src/hooks/useGetSuffix.ts
@@ -1,31 +1,36 @@
+export const useGetSuffix = (name: string, step: number): string => {
+  const lastChar = name.charAt(name.length - 1);
+  const lastCharCode = lastChar.charCodeAt(0);
 
-export const useGetSuffix = (name : string, step : number) => {
-    const lastChar = name.charAt(name.length - 1);
-    const lastCharCode = lastChar.charCodeAt(0);
-    if (lastCharCode < 44032 || lastCharCode > 55203) {
-      switch(step){
-        case 1:
-          return "는";
-        case 2:
-          return "가";
-        case 3:
-          return "와";
-        case 4:
-          return "를";
-        case 5:
-          return "는";
-      } 
-    }
-    switch(step){
+  if (lastCharCode < 44032 || lastCharCode > 55203) {
+    switch (step) {
       case 1:
-        return (lastCharCode - 44032) % 28 === 0 ? "는" : "은";
-      case 2:
-        return (lastCharCode - 44032) % 28 === 0 ? "가" : "이";
-      case 3:
-        return (lastCharCode - 44032) % 28 === 0 ? "와" : "과";
-      case 4:
-        return (lastCharCode - 44032) % 28 === 0 ? "를" : "을";
       case 5:
-        return (lastCharCode - 44032) % 28 === 0 ? "는" : "은";
+        return "는";
+      case 2:
+        return "가";
+      case 3:
+        return "와";
+      case 4:
+        return "를";
+      default:
+        return "는"; // 기본값 설정
     }
-  };
+  }
+
+  const isSuffixExists = (lastCharCode - 44032) % 28 !== 0;
+
+  switch (step) {
+    case 1:
+    case 5:
+      return isSuffixExists ? "은" : "는";
+    case 2:
+      return isSuffixExists ? "이" : "가";
+    case 3:
+      return isSuffixExists ? "과" : "와";
+    case 4:
+      return isSuffixExists ? "을" : "를";
+    default:
+      return isSuffixExists ? "은" : "는"; // 기본값 설정
+  }
+};
