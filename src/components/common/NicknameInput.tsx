32960d9f767d7f408a2008914ccc505c9ff0e854
조민규 <ariettys2@naver.com>
2024-08-17 09:26:31 +0900

    [fix.md 마무리] feat : 닉네임 버튼 눌렀을 때 비활성화 기능 추가

--- src/components/common/NicknameInput.tsx
+++ src/components/common/NicknameInput.tsx
@@ -3,7 +3,7 @@ import { postSignup } from "@/apis/host";
 import { useUserStore } from "@/store/user";
 import { inputReducer } from "@/types/Treducer";
 import { useRouter } from "next/router";
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import { styled } from "twin.macro";
 
 const NicknameInput = ({ hostName }: { hostName: string }) => {
@@ -11,8 +11,10 @@ const NicknameInput = ({ hostName }: { hostName: string }) => {
   const onChangeData = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch({ type: "CHANGE", payload: e.target.value });
   };
+  const [loading, setLoading] = useState(false);
   const route = useRouter();
   const onClick = async () => {
+    setLoading(true);
     if (route.pathname === "/") {
       let id: string | null = null;
       if (typeof window !== "undefined") {
@@ -23,15 +25,13 @@ const NicknameInput = ({ hostName }: { hostName: string }) => {
           hostId: id as string,
           guestName: data,
         });
-        if (result) {
+        if (result.data) {
           route.replace(
             `/guestQuestion?hostId=${id}&nickname=${data}&hostname=${hostName}`
           );
         }
-        dispatch({ type: "RESET" });
       } catch (error) {
         alert("닉네임이 중복됩니다");
-        dispatch({ type: "RESET" });
       }
     } else {
       let id: string | null = null;
@@ -52,12 +52,12 @@ const NicknameInput = ({ hostName }: { hostName: string }) => {
           });
           route.replace("/hostLoading");
         }
-        dispatch({ type: "RESET" });
       } catch (error) {
         alert("닉네임이 중복됩니다");
-        dispatch({ type: "RESET" });
       }
     }
+    setLoading(false);
+    dispatch({ type: "RESET" });
   };
   return (
     <>
@@ -70,8 +70,9 @@ const NicknameInput = ({ hostName }: { hostName: string }) => {
       />
       <div className=" font-Neo text-text-gray text-xs">한글 최대 15자</div>
       <button
-        className=" w-32 h-9 bg-main-color rounded-2xl text-white font-Neo font-bold mt-[20px]"
+        className=" w-32 h-9 bg-main-color rounded-2xl text-white font-Neo font-bold mt-[20px] disabled:bg-text-gray"
         onClick={onClick}
+        disabled={loading}
       >
         시작하기
       </button>
